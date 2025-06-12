ed3dea4d0e827f1ecf84e032ff7d8a2998b1a674
Shreeharsh <shindeshreeharsh157@gmail.com>
2025-06-12 18:26:32 +0530

    Learn Section Started with Header and User Progress

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,7 +1,21 @@
+import { FeedWrapper } from "@/components/feed-wrapper";
+import { StickyWrapper } from "@/components/sticky-wrapper";
+import { Header } from "./header";
+import { UserProgress } from "@/components/user-progress";
+
 const Learn = () => {
   return (
-    <div className="">
-      Learn Page
+    <div className="flex flex-row-reverse gap-[48px] px-6">
+    
+      <StickyWrapper>
+        <UserProgress activeCourse={{
+                  imageSrc: "/es.svg",
+                  title: "Spanish",
+              }} hearts={5} points={100} hasActiveSubscription={false} />
+      </StickyWrapper>
+      <FeedWrapper>
+        <Header title={"Spanish"} />
+      </FeedWrapper>
     </div>
   );
 }
---
+++ components/user-progress.tsx
@@ -0,0 +1,36 @@
+import Link from "next/link";
+import { Button } from "./ui/button";
+import Image from "next/image";
+import { InfinityIcon } from "lucide-react";
+
+type Props = {
+    activeCourse: {imageSrc: string; title: string;};
+    hearts: number;
+    points: number;
+    hasActiveSubscription: boolean;
+
+}
+
+export const UserProgress = ({activeCourse, points, hearts, hasActiveSubscription} : Props) => {
+    return (
+        <div className="flex items-center justify-between gap-x-2 w-full">
+            <Link href={"/courses"}>
+                <Button>
+                  <Image src={activeCourse.imageSrc} alt={activeCourse.title} width={32} height={32} className="rounded-md border" />
+                </Button>
+            </Link>
+            <Link href={"/shop"}>
+                <Button variant={"ghost"} className="text-orange-500">
+                    <Image src="/points.svg" alt="Points" width={28} height={28} className="mr-2" />
+                    {points}
+                </Button>
+            </Link>
+            <Link href={"/shop"}>
+                <Button variant={"ghost"} className="text-rose-500">
+                    <Image src="/heart.svg" alt="Hearts" width={22} height={22} className="mr-2" />
+                    {hasActiveSubscription ? <InfinityIcon className="h-4 w-4 stroke-[3]"/> : hearts}
+                </Button>
+            </Link>
+        </div>
+    );
+}
