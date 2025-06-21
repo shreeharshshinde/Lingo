496c0ae20669833546ba6a75ea408efed8e91554
Shreeharsh <shindeshreeharsh157@gmail.com>
2025-06-21 22:18:40 +0530

    modifying quiz

--- app/lesson/quiz.tsx
+++ app/lesson/quiz.tsx
@@ -18,6 +18,16 @@ type Props = {
 export const Quiz = ({initialHearts, initialLessonChallenges, initialLessonId, initialPercentage, userSubscription}: Props) => {
     const [hearts, setHearts] = useState(initialHearts)
     const [percentage, setPercentage] = useState(initialPercentage)
+    const [challenges] = useState(initialLessonChallenges);
+    const [activeIndex, useActiveIndex] = useState(() => {
+        const uncompletedIndex = challenges.findIndex((challenge) => !challenge.completed);
+        return uncompletedIndex === -1 ? 0 : uncompletedIndex;
+    })
+
+    const challenge = challenges[activeIndex];
+
+    const title = challenge.type === "ASSIST" ? "Select the Correct Meaning" : challenge.question
+
     return (
         <>
             <Header 
@@ -25,6 +35,19 @@ export const Quiz = ({initialHearts, initialLessonChallenges, initialLessonId, i
               percentage={percentage}
               hasActiveSubscription={!!userSubscription?.isActive}
             />
+            <div className="flex-1">
+                <div className="h-full flex items-center justify-center">
+                    <div className="lg:min-h-[350px] lg:w-[600px] w-full px-6 lg:px-0 flex flex-col gap-y-12">
+                        <h1 className="text-lg lg:text-3xl text-center lg:text-start font-bold text-neutral-700">
+                            {title}
+                        </h1>
+                        <div>
+
+                        </div>
+                    </div>
+
+                </div>
+            </div>
         </>
     )
 }
